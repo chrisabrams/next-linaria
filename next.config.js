6fd6386cfe09060b456b84142f3c6ce930492c0b
Chris Abrams <cabrams@godaddy.com>
2019-10-28 13:11:00 -0400

    simplify config

--- next.config.js
+++ next.config.js
@@ -1,63 +1,19 @@
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
-const { join } = require('path')
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/
-})
-const withPlugins = require('next-compose-plugins')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withPlugins([
-  [withMDX, {
-    pageExtensions: ['js', 'jsx', 'mdx']
-  }]
-], {
-  webpack: config => {
-
-    config.module.rules.forEach((rule) => {
-
-      // console.log(rule)
-
-      if(rule.test.toString().includes('js')) {
-
-        rule.use = [
-          rule.use,
-          {
-            loader: 'linaria/loader',
-            options: {
-              sourceMap: process.env.NODE_ENV !== 'production',
-            },
-          }
-        ]
-
-      }
-
-    })
-
+module.exports = withCSS({
+  webpack (config, options) {
     config.module.rules.push({
-      test: /\.css$/,
+      test: /\.(jsx?)$/,
       use: [
         {
-          loader: ExtractCssChunks.loader,
+          loader: 'linaria/loader',
           options: {
-            // only enable hot in development
-            hot: process.env.NODE_ENV === 'development',
-            // if hmr does not work, this is a forceful method.
-            reloadAll: true,
-          },
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: process.env.NODE_ENV !== 'production',
-          },
-        },
-      ],
+            sourceMap: process.env.NODE_ENV !== 'production'
+          }
+        }
+      ]
     })
 
-    config.plugins.push(new ExtractCssChunks({
-      filename: 'styles-[contenthash].css',
-    }))
-
     return config
-
   }
 })
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta charSet='utf-8' />
 
-          <link href="https://fonts.googleapis.com/css?family=:300,400,700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Cairo:300,400,700&display=swap" rel="stylesheet" />
           <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
 
           <script src="https://kit.fontawesome.com/a00dd54377.js"></script>
